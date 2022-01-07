56fab52c1903afa502519e473bacb25f9eb29210
Vignesh C <vigneshsb2008@gmail.com>
2022-01-07 12:39:11 +0530

    few changes

--- src/components/Invoice/TableEditor.jsx
+++ src/components/Invoice/TableEditor.jsx
@@ -1,6 +1,7 @@
-import React ,{useRef , useEffect , useState ,  
+import React, {
+    useRef, useEffect, useState,
     // useContext 
-}from 'react';
+} from 'react';
 import TextField from '@mui/material/TextField';
 
 // import { InvoiceDetailsContext } from '../../context/InvoiceDetailsContext';
@@ -10,6 +11,7 @@ export default function TableEditor(props) {
     const [tableVal, settableVal] = useState([])
     const data = [
         {
+            // id  : 0,
             itemName: "",
             quantity: "",
             price: "",
@@ -18,49 +20,62 @@ export default function TableEditor(props) {
     ]
 
     const dataVal = useRef(data)
+    const forceRender = useRef(false)
     const count = useRef(0)
-    dataVal.current = [...data];
+    dataVal.current = data
     const rowData = useRef('')
     // let str = ""
-    const onChangeHandler = (event, setFunction  , index) => {
-        event.preventDefault(); 
-        editTableRow(index , setFunction)
-    };
-    const onSaveHandler = (event  , index) => {
+    const onChangeHandler = (event, setFunction, index) => {
         event.preventDefault();
-        dataVal.current[index] =rowData.current
-        editTableRow(-1  , settableVal)
+        editTableRow(index, setFunction)
     };
-    const onRowEdit = (event,field) => {
-        rowData.current[field] = event.target.value 
+    const onSaveHandler = (event, index) => {
+        event.preventDefault();
+        dataVal.current[index] = rowData.current
+        editTableRow(-1, settableVal)
+    };
+    const onRowEdit = (event, field) => {
+        rowData.current[field] = event.target.value
+    };
+    const onInsertRow = (event) => {
+        event.preventDefault();
+        let currdata = dataVal.current;
+        console.log(" bef push")
+        console.log(currdata)
+        currdata.push(data[0])
+        console.log('currdata')
+        console.log(currdata)
+        dataVal.current = currdata;
+        editTableRow(-1, settableVal)
     };
 
 
-    function editTableRow(editindex , setFunction ) {
+
+    function editTableRow(editindex, setFunction) {
         tableData = dataVal.current.map((value, index) => {
             if (editindex !== index) {
-            return (    
-                <tr key={index}>
-                <td>{value.itemName}</td>
-                <td>{value.quantity}</td>
-                <td>{value.price}</td>
-                <td>{value.itemTotal}</td>
-                    <td>
-                        <button onClick={e => { onChangeHandler( e , settableVal , index) }}>Edit</button>
-                        <button onClick={e => { onChangeHandler( e , settableVal , index) }}>Delete</button>
-                    </td>
-                </tr>)
+                return (
+                    <tr key={index}>
+                        <td>{value.itemName}</td>
+                        <td>{value.quantity}</td>
+                        <td>{value.price}</td>
+                        <td>{value.itemTotal}</td>
+                        <td>
+                            <button onClick={e => { onChangeHandler(e, settableVal, index) }}>Edit</button>
+                            <button onClick={e => { onChangeHandler(e, settableVal, index) }}>Delete</button>
+                        </td>
+                    </tr>)
             } else {
                 rowData.current = value;
-                return (    
+                return (
                     <tr key={index}>
-                        <td><TextField className="right"  size="small"  variant="standard" onChange={e => onRowEdit(e, "itemName")} defaultValue={value.itemName} /></td>
-                        <td><TextField className="right"  size="small"  variant="standard" onChange={e => onRowEdit(e, "quantity")} defaultValue={value.quantity} /></td>
-                        <td><TextField className="right"  size="small"  variant="standard" onChange={e => onRowEdit(e, "price")} defaultValue={value.price} /></td>
-                        <td><TextField className="right"  size="small"  variant="standard" onChange={e => onRowEdit(e, "itemTotal")} defaultValue={value.itemTotal} /></td>
+                        <td><TextField className="right" size="small" variant="standard" onChange={e => onRowEdit(e, "itemName")} defaultValue={value.itemName} /></td>
+                        <td><TextField className="right" size="small" variant="standard" onChange={e => onRowEdit(e, "quantity")} defaultValue={value.quantity} /></td>
+                        <td><TextField className="right" size="small" variant="standard" onChange={e => onRowEdit(e, "price")} defaultValue={value.price} /></td>
+                        <td><TextField className="right" size="small" variant="standard" onChange={e => onRowEdit(e, "itemTotal")} defaultValue={value.itemTotal} /></td>
                         <td>
-                            <button onClick={e => { onSaveHandler( e ,  index) }}>Save</button>
-                            <button onClick={e => { onSaveHandler( e ,  index) }}>Cancel</button>
+                            <button onClick={e => { onSaveHandler(e, index) }}>Save</button>
+                            <button onClick={e => { onSaveHandler(e, index) }}>Cancel</button>
                         </td>
                     </tr>)
             }
@@ -69,7 +84,7 @@ export default function TableEditor(props) {
 
     }
     let renderRows = () => {
-        return  dataVal.current.map((value, index) => {
+        return dataVal.current.map((value, index) => {
             return (
                 <tr key={index}>
                     <td>{value.itemName}</td>
@@ -77,40 +92,43 @@ export default function TableEditor(props) {
                     <td>{value.price}</td>
                     <td>{value.itemTotal}</td>
                     <td>
-                        <button onClick={e => { onChangeHandler( e , settableVal , index) }}>Edit</button>
-                        <button onClick={e => { onChangeHandler( e , settableVal , index) }}>Delete</button>
+                        <button onClick={e => { onChangeHandler(e, settableVal, index) }}>Edit</button>
+                        <button onClick={e => { onChangeHandler(e, settableVal, index) }}>Delete</button>
                     </td>
                 </tr>)
         })
-    
+
     }
     let tableData = renderRows()
     // eslint-disable-next-line react-hooks/exhaustive-deps
     function rerender() {
-    if( !!props.value && props.value.length > 0) {
-
-        dataVal.current = props.value
-        if(count.current <= 2 && props.value.length > 0 ) {
+        if (!!props.value && props.value.length > 0) {
             dataVal.current = props.value
-            count.current = count.current + 1
-            tableData = renderRows()
+            if (count.current <= 2 && props.value.length > 0) {
+                dataVal.current = props.value
+                count.current = count.current + 1
+                tableData = renderRows()
 
-            settableVal(tableData)
-          
-        } else if (count <= 2) {
-            count.current = count.current + 1
-            tableData = renderRows()
-            settableVal(tableData)
-          
-            
+                settableVal(tableData)
+
+            } else if (count <= 2) {
+                count.current = count.current + 1
+                tableData = renderRows()
+                settableVal(tableData)
+            } else if (forceRender.current) {
+                forceRender.current = false
+                settableVal(tableData)
+            }
+
         }
     }
-}
+    
+    
     useEffect(() => {
         // settableVal(tableData)
         rerender()
         // eslint-disable-next-line
-    } , [rerender])
+    })
 
 
 
@@ -131,7 +149,7 @@ export default function TableEditor(props) {
                     {tableVal}
                 </tbody>
             </table>
-            {/* <button onClick={e => { onSaveHandler( e ,  index) }}>Cancel</button> */}
+            <button onClick={e => { onInsertRow(e, '') }}>Insert Row</button>
         </div>
     )
 }
